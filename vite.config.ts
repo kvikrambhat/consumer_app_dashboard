1502a342c9eff8d8d1562082a8b8becc0bbf51ff
vikrambhat-fluence <vikram.bhat@fluenceenergy.com>
2025-04-15 19:51:29 +0530

    using bit componet from the submodule

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@components': isProd
+        ? path.resolve(__dirname, 'node_modules/@bit-local-scope/components')
+        : path.resolve(__dirname, 'component-library/src/components'),
+    },
+  },
+});
