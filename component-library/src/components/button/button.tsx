b54dd43882fe5957d35c51773b04cc0edd47016f
vikrambhat-fluence <vikram.bhat@fluenceenergy.com>
2025-04-16 15:55:34 +0530

    Update to v0.0.4

--- component-library/src/components/button/button.tsx
+++ component-library/src/components/button/button.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-export type ButtonProps = {
+interface ButtonProps {
   label: string;
   onClick: () => void;
-};
+}
 
-export const Button = ({ label, onClick }: ButtonProps) => (
-  <button onClick={onClick}>{label}</button>
-);
+export const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+    return (
+      <button onClick={onClick} style={{ padding: "14px 28px", backgroundColor: "#ffeb3b" }}>
+        🚀 {label}
+      </button>
+    );
+  };
